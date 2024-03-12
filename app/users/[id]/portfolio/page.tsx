dec791a0f412bee4fdc3473d5b88f0c7d38acdd7
pokhrelashok2 <pokhrelashok2@gmail.com>
2024-03-12 14:35:49 +0545

    User portfolio updated

--- app/users/[id]/portfolio/page.tsx
+++ app/users/[id]/portfolio/page.tsx
@@ -12,6 +12,7 @@ function UserPortfolioPage() {
   const id = params.id;
   const { data, handle } = useInvoke<Portfolio>("get_user_portfolio");
   const { data: users } = useInvoke<User[]>("get_users", [], true);
+
   useEffect(() => {
     handle({ id });
   }, [handle, id]);
@@ -20,6 +21,11 @@ function UserPortfolioPage() {
     return users.find((u) => u.id == id);
   }, [users, id]);
 
+  const isGain = useMemo(() => {
+    if (!data) return false;
+    return data.totalValueOfLastTransPrice > data.totalValueOfPrevClosingPrice;
+  }, [data]);
+
   return (
     <Wrapper
       className="!py-0"
@@ -27,63 +33,106 @@ function UserPortfolioPage() {
       title={`${details ? details.name : "User"} Portfolio`}
     >
       {data ? (
-        <table className="min-w-full  border border-gray-300 z-[1111111] bg-blue-100">
-          <thead>
-            <tr className="sticky top-0 bg-yellow-100 border-b">
-              <th className="py-2 px-4 text-left"># </th>
-              <th className="py-2 px-4 text-left">Script</th>
-              <th className="py-2 px-4 text-left">Quantity</th>
-              <th className="py-2 px-4 text-left">Previous Value</th>
-              <th className="py-2 px-4 text-left">Latest Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.meroShareMyPortfolio.map((item, index) => {
-              return (
-                <tr
-                  key={item.script}
-                  className={`border-b ${
-                    item.valueOfPrevClosingPrice <= item.valueOfLastTransPrice
-                      ? item.valueOfPrevClosingPrice ==
-                        item.valueOfLastTransPrice
-                        ? "bg-blue-100"
-                        : "bg-green-100"
-                      : "bg-red-100"
-                  }`}
-                >
-                  <td className="py-2 px-4"> {index + 1} </td>
-                  <td className="py-2 px-4" title={item.scriptDesc}>
-                    {" "}
-                    {item.script}{" "}
-                  </td>
-                  <td className="py-2 px-4">{item.currentBalance}</td>
-                  <td className="py-2 px-4">
-                    Rs {formatPrice(parseFloat(item.valueOfPrevClosingPrice))}
-                  </td>
-                  <td className="py-2 px-4">
-                    Rs {formatPrice(parseFloat(item.valueOfLastTransPrice))}
-                  </td>
-                </tr>
-              );
-            })}
-            <tr className="font-bold sticky bottom-0 bg-yellow-100">
-              <td className="py-2 px-4"></td>
-              <td className="py-2 px-4"> Total </td>
-              <td className="py-2 px-4">
-                {data.meroShareMyPortfolio.reduce(
-                  (p, c) => c.currentBalance + p,
-                  0
+        <>
+          <div className="flex mb-4 flex-col sm:flex-row gap-2">
+            <div className="flex-1 flex flex-col items-center justify-center">
+              <h4 className="font-bold text-xl">Total Today</h4>
+              <p>Rs {formatPrice(data.totalValueOfLastTransPrice)}</p>
+            </div>
+            <div className="flex-1 flex flex-col items-center justify-center">
+              <h4 className="font-bold text-xl">
+                {isGain ? "Profit" : "Loss"} Today
+              </h4>
+              <p>
+                Rs{" "}
+                {formatPrice(
+                  Math.floor(
+                    Math.abs(
+                      data.totalValueOfLastTransPrice -
+                        data.totalValueOfPrevClosingPrice
+                    )
+                  )
                 )}
-              </td>
-              <td className="py-2 px-4">
-                Rs {formatPrice(data.totalValueOfPrevClosingPrice)}
-              </td>
-              <td className="py-2 px-4">
-                Rs {formatPrice(data.totalValueOfLastTransPrice)}
-              </td>
-            </tr>
-          </tbody>
-        </table>
+              </p>
+            </div>
+          </div>
+          <table className="min-w-full  border border-gray-300 z-[1111111] bg-blue-100">
+            <thead>
+              <tr className="sticky top-0 bg-yellow-100 border-b">
+                <th className="py-2 px-4 text-left"># </th>
+                <th className="py-2 px-4 text-left">Script</th>
+                <th className="py-2 px-4 text-left">Quantity</th>
+                <th className="py-2 px-4 text-left">LTP</th>
+                <th className="py-2 px-4 text-left">Total</th>
+                <th className="py-2 px-4 text-left">Gain/Loss</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.meroShareMyPortfolio.map((item, index) => {
+                return (
+                  <tr
+                    key={item.script}
+                    className={`border-b ${
+                      item.valueOfPrevClosingPrice <= item.valueOfLastTransPrice
+                        ? item.valueOfPrevClosingPrice ==
+                          item.valueOfLastTransPrice
+                          ? "bg-blue-100"
+                          : "bg-green-100"
+                        : "bg-red-100"
+                    }`}
+                  >
+                    <td className="py-2 px-4"> {index + 1} </td>
+                    <td className="py-2 px-4" title={item.scriptDesc}>
+                      {" "}
+                      {item.script}{" "}
+                    </td>
+                    <td className="py-2 px-4">{item.currentBalance}</td>
+                    <td className="py-2 px-4">
+                      Rs{" "}
+                      {formatPrice(
+                        Math.floor(
+                          parseFloat(item.valueAsOfLastTransactionPrice) /
+                            item.currentBalance
+                        )
+                      )}
+                    </td>
+                    <td className="py-2 px-4">
+                      Rs {formatPrice(parseFloat(item.valueOfLastTransPrice))}
+                    </td>
+                    <td className="py-2 px-4">
+                      Rs{" "}
+                      {formatPrice(
+                        Math.floor(
+                          Math.abs(
+                            parseFloat(item.valueOfPrevClosingPrice) -
+                              parseFloat(item.valueAsOfLastTransactionPrice)
+                          )
+                        )
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+              <tr className="font-bold sticky bottom-0 bg-yellow-100">
+                <td className="py-2 px-4"></td>
+                <td className="py-2 px-4"> Total </td>
+                <td className="py-2 px-4"></td>
+                <td className="py-2 px-4">
+                  {data.meroShareMyPortfolio.reduce(
+                    (p, c) => c.currentBalance + p,
+                    0
+                  )}
+                </td>
+                <td className="py-2 px-4">
+                  Rs {formatPrice(data.totalValueOfPrevClosingPrice)}
+                </td>
+                <td className="py-2 px-4">
+                  Rs {formatPrice(data.totalValueOfLastTransPrice)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
       ) : (
         <SectionLoading />
       )}
